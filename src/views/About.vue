aa5213b9b8d4cbeb57b3be74ee1c34210e613d28
barakdev <61583502+barakdev@users.noreply.github.com>
2020-03-01 13:30:26 +0100

    Add files via upload

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>About this proyect</h1>
+    <h3>Problems i found</h3>
+    <ul>
+      <li>Eslint loader didnt work properly with res to create a new AddTodo, so i coudnt use the page in the final step, SOLVED with () </li>
+      <li>When creating the id for the AddTodo, before using jsonPlaceHolder, it couldnt read the v4, showed 2 warnings maybe because of the version of the vue, i installed the latest one.</li>
+      <li>Couldnt build the dist folder, becuase of the eslint error, it wouldnt compile.</li>
+    </ul>  
+  </div>
+</template>
